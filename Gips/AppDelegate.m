3227a6aa7eb580d2ec6f12c5215e2281a0396cf3
Moaaz Sidat <moaaz.omer@gmail.com>
2015-08-18 19:33:47 -0400

    Initial Commit

---
+++ Gips/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Gips
+//
+//  Created by Moaaz Sidat on 2015-08-18.
+//  Copyright (c) 2015 MS. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Gips/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  Gips
+//
+//  Created by Moaaz Sidat on 2015-08-18.
+//  Copyright (c) 2015 MS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
